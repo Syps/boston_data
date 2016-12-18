b14aa420a8f1ca8f0d2b53063561ed246753dade
Syps <nsypteras@gmail.com>
2016-12-18 17:07:49 -0500

    add favicon, remove unused fonts

--- site/server.js
+++ site/server.js
@@ -1,11 +1,13 @@
 var express      = require('express');
 var app          = express();
-
+var favicon      = require('serve-favicon');
 
 app.engine('html', require('ejs').renderFile);
 app.set('views', __dirname + '/views')
 app.use('/', express.static(__dirname + '/public/'))
+app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.set('port', (process.env.PORT || 3000))
+
 
 app.get('/neighborhoods.json', (req,res) => {
 	neighborhoods = require('./public/json/neighborhoods.json');
